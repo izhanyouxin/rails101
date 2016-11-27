05901102bf3c172ee37d7954095a7d36be758081
zhanyouxin <zhanyouxin@gmail.com>
2016-11-27 10:10:32 +0800

    add bootstrap to project

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,2 +1,10 @@
 class WelcomeController < ApplicationController
+  def index
+    flash[:alert] = "晚安！该睡了！"
+  end
+end
+class WelcomeController < ApplicationController
+  def index
+   flash[:warning] = "这是 warning 讯息！"
+  end
 end
